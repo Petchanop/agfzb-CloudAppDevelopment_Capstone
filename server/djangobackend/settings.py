0714667fa9741e399e39bd570915a79e49b294ac
Noppadon Piya-isarakul <npiya-is@z2t10c4.42bangkok.com>
2022-03-21 21:21:38 +0700

    add localhost

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["bestcar.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["localhost","bestcar.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
