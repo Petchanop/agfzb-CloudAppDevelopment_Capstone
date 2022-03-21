74166cdbdd3e8fc6c5eb3f512b930a743e67d46f
Noppadon Piya-isarakul <npiya-is@z2t10c4.42bangkok.com>
2022-03-22 01:20:24 +0700

    add url view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -33,6 +33,21 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'onlinecourse/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
