b3bf28cdf68436955149159188d2248dca01f13b
Noppadon Piya-isarakul <npiya-is@z2t10c4.42bangkok.com>
2022-03-21 18:29:22 +0700

    3rd try

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='' ,view=views.about, name='about'),
+    path(route='djangoapp/about.html' ,view=views.about, name='about'),
     # path for contact us view
 
     # path for registration
